b9ebfb8361df66fafc6613c93cc9e9680202a4a2
DennisChicaizaA <dennis.chicaiza.a@gmail.com>
2022-10-08 17:33:43 -0500

    Feat: first mini calculator UI version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mini-calculator';
+  title = 'Mini Calculator';
+
+  numberOne: number = 0;
+  numberTwo: number = 0;
+  result: number = 0;
+
+  add():void{
+    this.result = Number(this.numberOne) + Number(this.numberTwo);
+  }
+
+  substraction():void{
+    this.result = Number(this.numberOne) - Number(this.numberTwo);
+  }
+
+  multiply():void{
+    this.result = Number(this.numberOne) * Number(this.numberTwo);
+  }
+
+  divide():void{
+    this.result = Number(this.numberOne) / Number(this.numberTwo);
+  }
 }
