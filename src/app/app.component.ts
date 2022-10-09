57cba82b80ac5de498bcfbf297405ef2f938b52a
DennisChicaizaA <dennis.chicaiza.a@gmail.com>
2022-10-09 15:50:26 -0500

    Feat: enhancing UI for the mini calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Self } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,25 +6,158 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'Mini Calculator';
 
-  numberOne: number = 0;
-  numberTwo: number = 0;
+  numbers: { [key: string]: string } = {"one":"1","two":"2","three":"3","four":"4","five":"5",
+                                          "six":"6","seven":"7","eight":"8","nine":"9","zero":"0"};
+  opers:{ [key: string]: string } = {"add":"+","sub":"-","multiply":"*","divide":"/"};
+  oper: string = "0";
   result: number = 0;
+  history: string[] = [];
 
-  add():void{
-    this.result = Number(this.numberOne) + Number(this.numberTwo);
+  clean(){
+    this.oper = "0";
+    this.result = 0;
   }
 
-  substraction():void{
-    this.result = Number(this.numberOne) - Number(this.numberTwo);
+  setOne(){
+    if (this.oper == this.numbers["zero"]) {
+      this.oper="";
+      this.oper+= this.numbers["one"]
+    } else {
+      this.oper+= this.numbers["one"]
+    }
+  }
+
+  setTwo(){
+    if (this.oper == this.numbers["zero"]) {
+      this.oper="";
+      this.oper+= this.numbers["two"]
+    } else {
+      this.oper+= this.numbers["two"]
+    }
+  }
+
+  setThree(){
+    if (this.oper == this.numbers["zero"]) {
+      this.oper="";
+      this.oper+= this.numbers["three"]
+    } else {
+      this.oper+= this.numbers["three"]
+    }
+  }
+
+  setFour(){
+    if (this.oper == this.numbers["zero"]) {
+      this.oper="";
+      this.oper+= this.numbers["four"]
+    } else {
+      this.oper+= this.numbers["four"]
+    }
+  }
+
+  setFive(){
+    if (this.oper == this.numbers["zero"]) {
+      this.oper="";
+      this.oper+= this.numbers["five"]
+    } else {
+      this.oper+= this.numbers["five"]
+    }
+  }
+
+  setSix(){
+    if (this.oper == this.numbers["zero"]) {
+      this.oper="";
+      this.oper+= this.numbers["six"]
+    } else {
+      this.oper+= this.numbers["six"]
+    }
+  }
+
+  setSeven(){
+    if (this.oper == this.numbers["zero"]) {
+      this.oper="";
+      this.oper+= this.numbers["seven"]
+    } else {
+      this.oper+= this.numbers["seven"]
+    }
+  }
+
+  setEight(){
+    if (this.oper == this.numbers["zero"]) {
+      this.oper="";
+      this.oper+= this.numbers["eight"]
+    } else {
+      this.oper+= this.numbers["eight"]
+    }
+  }
+
+  setNine(){
+    if (this.oper == this.numbers["zero"]) {
+      this.oper="";
+      this.oper+= this.numbers["nine"]
+    } else {
+      this.oper+= this.numbers["nine"]
+    }
+  }
+
+  setZero(){
+    if (this.oper != this.numbers["zero"]) {
+      this.oper+= this.numbers["zero"];
+    }
+  }
+
+  add():void{
+    if (this.oper != this.numbers["zero"] && Number(this.oper)) { //Number(value) checks if it is a valid number
+      this.oper+= " " + this.opers["add"] + " ";
+    }
+  }
+
+  substract():void{
+    if (this.oper != this.numbers["zero"] && Number(this.oper)) { //Number(value) checks if it is a valid number
+      this.oper+= " " + this.opers["sub"] + " ";
+    }
   }
 
   multiply():void{
-    this.result = Number(this.numberOne) * Number(this.numberTwo);
+    if (this.oper != this.numbers["zero"] && Number(this.oper)) { //Number(value) checks if it is a valid number
+      this.oper+= " " + this.opers["multiply"] + " ";
+    }
   }
 
   divide():void{
-    this.result = Number(this.numberOne) / Number(this.numberTwo);
+    if (this.oper != this.numbers["zero"] && Number(this.oper)) { //Number(value) checks if it is a valid number
+      this.oper+= " " + this.opers["divide"] + " ";
+    }
+  }
+
+  equals():void{
+    let splitted = this.oper.split(" ", 3);
+    switch (splitted[1]) {
+      case "+":
+        this.result = Number(splitted[0]) + Number(splitted[2]);
+        break;
+      case "-":
+        this.result = Number(splitted[0]) - Number(splitted[2]);
+        break;
+      case "*":
+        this.result = Number(splitted[0]) * Number(splitted[2]);
+        break;
+      case "/":
+        this.result = Number(splitted[0]) / Number(splitted[2]);
+        break;
+      default:
+        break;
+    }
+    splitted.push('=', String(this.result));
+    let hist = ""
+    splitted.forEach(value => {
+      hist+=" " + value + " ";
+    });
+    this.history.push(hist);
+    this.oper = "0";
+    console.log(this.history);
+    
   }
 }
